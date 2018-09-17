4ebacab0f874f16874afc4c0a650869d8c941b3a
qweerer <liu2946181970@126.com>
2018-09-17 16:38:09 +0800

    PJ12 问题2.1

--- 项目12中国城市资本流动问题探索/PJ12.py
+++ 项目12中国城市资本流动问题探索/PJ12.py
@@ -8,8 +8,8 @@ import seaborn as sns
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 步骤一（替换sans-serif字体）
 plt.rcParams['axes.unicode_minus'] = False    # 步骤二（解决坐标轴负数的负号显示问题）
 # sns.set_style("ticks", {"font.sans-serif": ['simhei', 'Droid Sans Fallback']})
-# os.chdir('D:\\code\\2018spyder\\项目12中国城市资本流动问题探索')
-os.chdir('D:\\user\\Documents\\00code\\2018spyder\\项目12中国城市资本流动问题探索')
+os.chdir('D:\\code\\2018spyder\\项目12中国城市资本流动问题探索')
+# os.chdir('D:\\user\\Documents\\00code\\2018spyder\\项目12中国城市资本流动问题探索')
 print('导入模块成功')
 # %%
 data = pd.read_excel('data.xlsx', sheet_name='Sheet1')
@@ -46,24 +46,8 @@ plt.grid(linestyle='--', linewidth=1, axis='y', alpha=0.5)
 ax2.legend(loc='best', fontsize='xx-large')
 plt.title('异地投资')
 
-# %%Q1.1删除
-# del dataQ11SamTu,dataQ11DifTu
-# %%Q1.2图的数据
-figQ1 = plt.figure(figsize=(14, 20))
-plt.subplots_adjust(hspace=0.5)
-x = 0
-while x < 8:
-    x = x + 1
-    print(x)
-    ax1 = figQ1.add_subplot(4, 2, x)
-    dataQ11SamTu.plot(kind='bar', ax=ax1,
-                      facecolor='yellowgreen', alpha=0.8,
-                      edgecolor='black', linewidth=2)
-    plt.grid(linestyle='--', linewidth=1, axis='y', alpha=0.5)
-    ax1.legend(loc='best')
-    plt.title('同城投资')
 
-# %%
+# %%Q1.2图的数据
 
 figQ1 = plt.figure(figsize=(14, 20))
 plt.subplots_adjust(hspace=0.5)
@@ -94,4 +78,17 @@ for i, j in dataQ11Sam.groupby('年份'):
     ax1.legend(loc='best')
     plt.title('同城投资_%i' % i)
 del i, j, x
+# %%Q1.1删除
+del dataQ11SamTu, dataQ11DifTu, dataQ11Sam, dataQ11Dif
 # %%
+dataCity = pd.read_excel('中国城市代码对照表.xlsx', sheet_name='Sheet1')
+dataQ2 = data[['投资方所在城市', '融资方所在城市', '投资企业对数']].groupby(['投资方所在城市', '融资方所在城市']).sum()
+dataQ2 = dataQ2.reset_index()
+
+dataQ2 = pd.merge(dataQ2, dataCity[['城市名称', '经度', '纬度']], left_on='投资方所在城市', right_on='城市名称')
+del dataQ2['城市名称']
+dataQ2.rename(columns={'经度': 'tzLng', '纬度': 'tzLat'}, inplace=True)
+
+dataQ2 = pd.merge(dataQ2, dataCity[['城市名称', '经度', '纬度']], left_on='融资方所在城市', right_on='城市名称')
+del dataQ2['城市名称']
+dataQ2.rename(columns={'经度': 'rzLng', '纬度': 'rzLat'}, inplace=True)
