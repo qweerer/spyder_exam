5448efbe8c37fb962657e28c68d957686c755d70
qweerer <liu2946181970@126.com>
2018-09-19 11:05:31 +0800

    PJ12 Q3.1

--- 项目12中国城市资本流动问题探索/PJ12.py
+++ 项目12中国城市资本流动问题探索/PJ12.py
@@ -9,8 +9,8 @@ import seaborn as sns
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 步骤一（替换sans-serif字体）
 plt.rcParams['axes.unicode_minus'] = False    # 步骤二（解决坐标轴负数的负号显示问题）
 # sns.set_style("ticks", {"font.sans-serif": ['simhei', 'Droid Sans Fallback']})
-# os.chdir('D:\\code\\2018spyder\\项目12中国城市资本流动问题探索')
-os.chdir('D:\\user\\Documents\\00code\\2018spyder\\项目12中国城市资本流动问题探索')
+os.chdir('D:\\code\\2018spyder\\项目12中国城市资本流动问题探索')
+# os.chdir('D:\\user\\Documents\\00code\\2018spyder\\项目12中国城市资本流动问题探索')
 print('导入模块成功')
 # %%
 data = pd.read_excel('data.xlsx', sheet_name='Sheet1')
@@ -94,7 +94,7 @@ dataQ2 = pd.merge(dataQ2, dataCity[['城市名称', '经度', '纬度']], left_o
 del dataQ2['城市名称']
 dataQ2.rename(columns={'经度': 'rzLng', '纬度': 'rzLat'}, inplace=True)
 # %%Q2.2
-dataQ22 = dataQ11Dif[['投资方所在城市', '融资方所在城市', '投资企业对数']]
+dataQ22 = dataQ11Dif[['投资方所在城市', '融资方所在城市', '投资企业对数']].copy()
 dataQ22.columns = ['source', 'target', 'weight']
 dataQ22['weight'] = (dataQ22['weight'] - dataQ22['weight'].min()) / (dataQ22['weight'].max() - dataQ22['weight'].min())
 dataQ22.to_csv('./输出/Q22Line.csv', index=0, encoding='utf_8')
@@ -109,4 +109,50 @@ dataQ22Point.to_csv('./输出/Q22Point.csv', index=0, encoding='utf_8')
 dataQ2.to_csv('./输出/Q22Qgis.csv', index=0, encoding='utf_8')
 # %%Q2删除
 del dataQ22, dataQ22Point
+# %%
+dataQ3Stake = dataQ11Dif[['投资方所在城市', '投资企业对数']].groupby('投资方所在城市').sum().sort_values('投资企业对数', ascending=False).reset_index()[:10]
+dataQ3Attract = dataQ11Dif[['融资方所在城市', '投资企业对数']].groupby('融资方所在城市').sum().sort_values('投资企业对数', ascending=False).reset_index()[:10]
+dataQ3Stake.columns = ['A','value']
+dataQ3Attract.columns = ['A','value']
+
+dataQ3List = pd.concat([dataQ3Stake,dataQ3Attract])
+dataQ3List = dataQ3List.groupby('A').count()
+
+dataQ3 = pd.merge(dataQ3List,dataQ2,left_on='A',right_on='投资方所在城市',how = 'outer')
+dataQ3 = pd.merge(dataQ3List,dataQ3,left_on='A',right_on='融资方所在城市',how = 'outer')
+dataQ3 = dataQ3[(dataQ3['value_x']>0) | (dataQ3['value_y']>0)]
+del dataQ3['value_x'], dataQ3['value_y']
+
+
+# %%
+dataQ3Stake.index = dataQ3Stake['A']
+dataQ3Attract.index = dataQ3Attract['A']
+
+figQ31 = plt.figure(figsize=(8, 7))
+plt.subplots_adjust(hspace=0.3)
+
+ax1 = figQ31.add_subplot(2, 1, 1)
+dataQ3Stake['value'].plot(kind='bar', ax=ax1,
+                          facecolor='yellowgreen', alpha=0.8,
+                          edgecolor='black', linewidth=2)
+plt.grid(linestyle='--', linewidth=1, axis='y', alpha=0.5)
+plt.title('投资方所在城市')
+
+
+ax2 = figQ31.add_subplot(2, 1, 2)
+dataQ3Attract['value'].plot(kind='bar', ax=ax2,
+                            facecolor='lightskyblue', alpha=0.8,
+                            edgecolor='black', linewidth=2)
+plt.grid(linestyle='--', linewidth=1, axis='y', alpha=0.5)
+plt.title('投资方所在城市')
+
+
+
+
+
+
+
+
+
+
 
