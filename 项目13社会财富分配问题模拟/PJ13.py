3e60ff428bc2f0c354eb16347dfe450f695651a8
qweerer <liu2946181970@126.com>
2018-09-29 09:49:11 +0800

    PJ13 Q1函数

--- 项目13社会财富分配问题模拟/PJ13.py
+++ 项目13社会财富分配问题模拟/PJ13.py
@@ -23,6 +23,84 @@ fortune = pd.merge(fortune,gain_r1,left_index = True,right_index = True,how = 'o
 fortune = fortune.fillna(0)
 # 合并数据，得到这一轮每个人“盈”多少钱
 fortune[1] = fortune[1]+fortune['+']
-del fortune['+']
+del fortune['+'],gain_r1
 
 # %%
+def process (dataf,round_i):
+    data = dataf.copy()
+    if len(data[data[round_i-1] == 0] >0):
+        datac = data[round_i-1][data[round_i-1]>0]
+        # 财富值大于0的全部减1
+        datac[round_i] = datac[round_i-1]-1
+        # 财富分配，统计
+        longc = len(datac)
+        given = pd.DataFrame({'given':np.random.choice(person_n,size=longc)})
+        given['+'] = 1
+        given = given.groupby('given').count()
+        # 财富分配与财富值大于0的合并，
+        # 采取‘outer’可以使财富值小于0的分配也包含在内，但是需要填充Nan值
+        datac = pd.merge(datac,given,left_index=True,right_index=True,how='outer')
+        datac = datac.fillna(0)
+        datac[round_i] = datac[round_i]+ datac['+']
+        # datac与data(源数据)合并
+        data = pd.merge(data,datac[[3,'+']],left_index=True,right_index=True,how='outer')
+        data = data.fillna(0)
+
+    else:
+        data[round_i] = data[round_i-1]-1
+
+        given = pd.DataFrame({'given':np.random.choice(person_n,100)})
+        given['+'] = 1
+        given = given.groupby('given').count()
+        
+        data = pd.merge(data,given,left_index = True,right_index = True,how = 'outer')
+        data = data.fillna(0)
+        data[round_i] = data[round_i] + data['+']
+        
+    return data[round_i]
+# %%
+del fortune[2]
+fortune[2] = pd.DataFrame(process(fortune,2))
+print(fortune[2].sum())
+
+#%%
+
+ccc = pd.DataFrame({'c':np.random.choice([0,1],size=100)},
+                    index = person_n)
+data = fortune.copy()
+data[2][ccc['c'] ==  0] = 0
+print(data[2].sum())
+# %%
+
+datac = pd.DataFrame(data[2][data[2]>0])
+        # 财富值大于0的全部减1
+datac[3] = datac[2]-1
+        # 财富分配，统计
+longc = len(datac)
+given = pd.DataFrame({'given':np.random.choice(person_n,size=longc)})
+given['+'] = 1
+given = given.groupby('given').count()
+
+        # 财富分配与财富值大于0的合并，
+        # 采取‘outer’可以使财富值小于0的分配也包含在内，但是需要填充Nan值
+datac = pd.merge(datac,given,left_index=True,right_index=True,how='outer')
+datac = datac.fillna(0)
+datac[3] = datac[3]+ datac['+']
+data = pd.merge(data,datac[[3,'+']],left_index=True,right_index=True,how='outer')
+data = data.fillna(0)
+print(data[3].sum())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
