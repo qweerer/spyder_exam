668829873d341ec1cbda48730b4a6e7d3aecebdd
liuning <liu2946181970@126.com>
2018-10-10 11:33:34 +0800

    PJ15 Q1.3

--- 项目15泰坦尼克号获救问题/PJ15.py
+++ 项目15泰坦尼克号获救问题/PJ15.py
@@ -13,11 +13,13 @@ import seaborn as sns
 
 # 导入图表绘制、图标展示模块
 
-sns.set_style("ticks",{"font.sans-serif":['simhei','Droid Sans Fallback']})
+mpl.rcParams['font.sans-serif'] = ['SimHei'] # 指定默认字体
+sns.set_style("ticks",{"font.sans-serif":['simhei','Arial']})
+sns.set_context("talk")
 plt.rcParams['axes.unicode_minus'] = False
 
 # os.chdir('D:/user/Documents/00code/spyder_exam/项目15泰坦尼克号获救问题/输出')
-os.chdir('/home/qweerer/0code/spyder_exam/项目15泰坦尼克号获救问题')
+# os.chdir('/home/qweerer/0code/spyder_exam/项目15泰坦尼克号获救问题')
 
 # pathPj15 = os.path.abspath('.')
 print('导入模块完成')
