6b2b54d8f08a8ca4ba16f5d0f730c124bbe6aeda
LiuNing <liu2946181970@126.com>
2018-10-12 16:49:10 +0800

    PJ15 Q4完成
    完成第3问：亲戚与是否幸存的关系
    完成第4问：船票价格与是否幸存的关系

--- 项目15泰坦尼克号获救问题/PJ15.py
+++ 项目15泰坦尼克号获救问题/PJ15.py
@@ -147,5 +147,147 @@ sns.barplot(x=picDataQ24.index, y="survived", data=picDataQ24,
 plt.grid(color='grey', linestyle=':', linewidth=1, axis='both')
 plt.savefig('PJ15Q2.4.png', dpi=200)
 del picDataQ24, picDataQ2
-# %% 3
+# %% 3.1
 picDataQ3 = trainData[['Survived', 'SibSp', 'Parch']]
+
+picDataQ3['Survived'][picDataQ3['Survived'] == 1] = '幸存'
+picDataQ3['Survived'][picDataQ3['Survived'] == 0] = '死亡'
+
+figQ31 = plt.figure(figsize=(15, 3))
+plt.subplots_adjust(wspace=0.1)
+
+picDataQ31 = picDataQ3[['Survived', 'SibSp']][picDataQ3['SibSp'] > 0].groupby('Survived').count()
+ax1 = figQ31.add_subplot(1, 4, 1)
+plt.title('有兄弟姐妹存活比例')
+plt.axis('equal')
+plt.pie(picDataQ31,
+        labels=picDataQ31.index,
+        colors=['#33CCCC', '#057D9F'],
+        autopct='%.2f%%',
+        startangle=90,
+        wedgeprops={'edgecolor': 'black', 'linestyle': '-.'})
+
+picDataQ31 = picDataQ3[['Survived', 'SibSp']][picDataQ3['SibSp'] == 0].groupby('Survived').count()
+ax1 = figQ31.add_subplot(1, 4, 2)
+plt.title('无兄弟姐妹存活比例')
+plt.axis('equal')
+plt.pie(picDataQ31,
+        labels=picDataQ31.index,
+        colors=['#33CCCC', '#057D9F'],
+        autopct='%.2f%%',
+        startangle=90,
+        wedgeprops={'edgecolor': 'black', 'linestyle': '-.'})
+
+picDataQ31 = picDataQ3[['Survived', 'Parch']][picDataQ3['Parch'] > 0].groupby('Survived').count()
+ax1 = figQ31.add_subplot(1, 4, 3)
+plt.title('有父母子女存活比例')
+plt.axis('equal')
+plt.pie(picDataQ31,
+        labels=picDataQ31.index,
+        colors=['#FF8540', '#FFA573'],
+        autopct='%.2f%%',
+        startangle=90,
+        wedgeprops={'edgecolor': 'black', 'linestyle': '-.'})
+
+picDataQ31 = picDataQ3[['Survived', 'Parch']][picDataQ3['Parch'] == 0].groupby('Survived').count()
+ax1 = figQ31.add_subplot(1, 4, 4)
+plt.title('无父母子女存活比例')
+plt.axis('equal')
+plt.pie(picDataQ31,
+        labels=picDataQ31.index,
+        colors=['#FF8540', '#FFA573'],
+        autopct='%.2f%%',
+        startangle=90,
+        wedgeprops={'edgecolor': 'black', 'linestyle': '-.'})
+
+plt.savefig('PJ15Q3.1.png', dpi=200)
+
+del picDataQ31
+# %% 3.2
+picDataQ3 = trainData[['Survived', 'SibSp', 'Parch']]
+
+figQ32 = plt.figure(figsize=(10, 4))
+plt.subplots_adjust(wspace=0.25)
+
+ax1 = figQ32.add_subplot(1, 2, 1)
+
+picDataQ32 = picDataQ3[['Survived', 'SibSp']].groupby('SibSp').agg(['count', 'sum'])
+picDataQ32.columns = ['count', 'sum']
+picDataQ32['survived'] = picDataQ32['sum'] / picDataQ32['count']
+picDataQ32.index.name = '兄弟姐妹数量'
+
+sns.barplot(x=picDataQ32.index, y="survived", data=picDataQ32,
+            palette='Blues', edgecolor='black')
+picDataQ32['count'].plot(kind='line', color='#057D9F',
+                         marker='o', linestyle='--', secondary_y=True)
+plt.legend(loc='upper right')
+plt.grid(color='grey', linestyle=':', linewidth=1, axis='y')
+plt.title('不同兄弟姐妹数量的存活比例')
+
+
+ax2 = figQ32.add_subplot(1, 2, 2)
+
+picDataQ32 = picDataQ3[['Survived', 'Parch']].groupby('Parch').agg(['count', 'sum'])
+picDataQ32.columns = ['count', 'sum']
+picDataQ32['survived'] = picDataQ32['sum'] / picDataQ32['count']
+picDataQ32.index.name = '父母子女数量'
+
+sns.barplot(x=picDataQ32.index, y="survived", data=picDataQ32,
+            palette='OrRd', edgecolor='black')
+picDataQ32['count'].plot(kind='line', color='#FF8540',
+                         marker='o', linestyle='--', secondary_y=True)
+plt.legend(loc='upper right')
+plt.grid(color='grey', linestyle=':', linewidth=1, axis='y')
+plt.title('不同父母子女数量的存活比例')
+
+plt.savefig('PJ15Q3.2.png', dpi=200)
+
+# 总人数图表
+figQ33 = plt.figure(figsize=(10, 4))
+
+picDataQ3['All'] = picDataQ3['SibSp'] + picDataQ3['Parch']
+
+picDataQ32 = picDataQ3[['Survived', 'All']].groupby('All').agg(['count', 'sum'])
+picDataQ32.columns = ['count', 'sum']
+picDataQ32['survived'] = picDataQ32['sum'] / picDataQ32['count']
+picDataQ32.index.name = '亲戚数量'
+
+sns.barplot(x=picDataQ32.index, y="survived", data=picDataQ32,
+            palette='BuPu', edgecolor='black')
+picDataQ32['count'].plot(kind='line', color='purple',
+                         marker='o', linestyle='--', secondary_y=True)
+plt.legend(loc='upper right')
+plt.grid(color='grey', linestyle=':', linewidth=1, axis='y')
+plt.title('不同亲戚数量的存活比例')
+
+
+plt.savefig('PJ15Q3.3.png', dpi=200)
+
+del picDataQ32, picDataQ3
+# %%
+picDataQ4 = trainData[['Survived', 'Pclass', 'Fare']]
+
+figQ41 = plt.figure(figsize=(16, 6))
+plt.subplots_adjust(wspace=0.2)
+
+ax1 = figQ41.add_subplot(1, 2, 1)
+plt.hist(picDataQ4['Fare'], bins=100, histtype='bar',
+         edgecolor='black', alpha=0.7)
+plt.grid(color='grey', linestyle=':', linewidth=1, axis='both')
+
+
+ax2 = figQ41.add_subplot(1, 2, 2)
+sns.boxplot(x='Pclass', y='Fare', hue='Survived', data=picDataQ4,
+            width=0.3, fliersize=3, palette=['#FFCCCC', '#0099CC'])
+plt.ylim(0, 230)
+plt.savefig('PJ15Q4.1.png', dpi=200)
+
+figQ42 = plt.figure(figsize=(16, 6))
+sns.boxplot(x='Fare', y='Survived', data=picDataQ4,
+            width=0.3, fliersize=3, palette=['#FFCCCC', '#0099CC'],
+            orient="h")
+plt.savefig('PJ15Q4.2.png', dpi=200)
+
+del picDataQ4
+
+# %%
